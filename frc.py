4c828cea67f726fc01a68cf7a961d9a340a5805d
Andrew Gazelka <andrew.gazelka@gmail.com>
2019-01-10 12:45:24 -0600

    add documentation for most of frc.py

--- frc.py
+++ frc.py
@@ -19,59 +19,79 @@ VISION_TAPE_MIN_SEPARATION_IN = 8
 VISION_TAPE_MIN_SEPARATION_FT = VISION_TAPE_MIN_SEPARATION_IN / 12
 
 VISION_TAPE_ANGLE_FROM_VERT_DEG = 14.5
+VISION_TAPE_ANGLE_FROM_VERT_RAD = math.radians(VISION_TAPE_ANGLE_FROM_VERT_DEG)
 
 VISION_TAPE_ANGLE_FROM_HORIZONTAL_DEG = 90 - VISION_TAPE_ANGLE_FROM_VERT_DEG
-
 VISION_TAPE_ANGLE_FROM_HORIZONTAL_RAD = math.radians(VISION_TAPE_ANGLE_FROM_HORIZONTAL_DEG)
 
 REAL_HEIGHT_FT = VISION_TAPE_LENGTH_FT * math.sin(VISION_TAPE_ANGLE_FROM_HORIZONTAL_RAD)
-TOP_WIDTH_FT = (2 * VISION_TAPE_WIDTH_FT * math.sin(VISION_TAPE_ANGLE_FROM_HORIZONTAL_RAD) +
-                VISION_TAPE_MIN_SEPARATION_FT)
+TOP_WIDTH_FT = (2 * VISION_TAPE_WIDTH_FT * math.sin(VISION_TAPE_ANGLE_FROM_HORIZONTAL_RAD) + VISION_TAPE_MIN_SEPARATION_FT)
 
-BOTTOM_WIDTH_FT = (2 * VISION_TAPE_LENGTH_FT * math.sin(math.radians(VISION_TAPE_ANGLE_FROM_VERT_DEG)) +
-                   VISION_TAPE_MIN_SEPARATION_FT)
+BOTTOM_WIDTH_FT = (2 * VISION_TAPE_LENGTH_FT * math.sin(VISION_TAPE_ANGLE_FROM_VERT_RAD) + VISION_TAPE_MIN_SEPARATION_FT)
 
 MID_WIDTH_FT = (TOP_WIDTH_FT + BOTTOM_WIDTH_FT) / 2
 
-# TODO: please explain and add variable constant
+# Distance from top left corner to bottom right corner
 DIAG_WIDTH_FT = np.linalg.norm(
-    np.array([BOTTOM_WIDTH_FT, 0]).T +
-    np.array([VISION_TAPE_WIDTH_FT * math.cos(math.radians(14.5)), VISION_TAPE_WIDTH_FT * math.sin(math.radians(14.5))]) +
-    np.array([-VISION_TAPE_LENGTH_FT * math.sin(math.radians(14.5)), VISION_TAPE_LENGTH_FT * math.cos(math.radians(14.5))])
+    np.array([BOTTOM_WIDTH_FT, 0]) +
+    np.array([VISION_TAPE_WIDTH_FT * math.cos(VISION_TAPE_ANGLE_FROM_VERT_RAD), VISION_TAPE_WIDTH_FT * math.sin(VISION_TAPE_ANGLE_FROM_VERT_RAD)]) +
+    np.array([-VISION_TAPE_LENGTH_FT * math.sin(math.radians(VISION_TAPE_ANGLE_FROM_VERT_DEG)), VISION_TAPE_LENGTH_FT * math.cos(VISION_TAPE_ANGLE_FROM_VERT_RAD)])
 )
 
 
-BLENDER_FLEN = 800.6028523694872235460223543952119609884957052979378391303  # (211 * 1.83283) / REAL_HEIGHT_FT
+# The focal length of the camera in Blender (used to create autogen_images).
+# Calculated with https://www.pyimagesearch.com/2015/01/19/find-distance-camera-objectmarker-using-python-opencv/
+BLENDER_FLEN_PIXELS = 800.6028523694872235460223543952119609884957052979378391303  # (211 * 1.83283) / REAL_HEIGHT_FT
 
-print("BLENDER_FLEN=", BLENDER_FLEN)
+BLENDER_FLEN_MM = 35
+
+BLENDER_SENSOR_WIDTH_MM = 32
+BLENDER_SENSOR_WIDTH_FT = BLENDER_SENSOR_WIDTH_MM * 0.00328084
+
+BLENDER_SENSOR_HEIGHT_MM = 18
+BLENDER_SENSOR_HEIGHT_FT = BLENDER_SENSOR_HEIGHT_MM * 0.00328084
+
+BLENDER_FLEN_FT = BLENDER_FLEN_MM * 0.00328084
+
+BLENDER_PIXEL_WIDTH = 1440
+BLENDER_PIXEL_HEIGHT = 720
 
 
-def get_dist(height):
-    return (REAL_HEIGHT_FT * BLENDER_FLEN) / height
+def get_dist_ft(height_px):
+    """
+    
+    :param height_px: pixel distance from the lowest to the highest point on the vision target
+    :return: the distance in feet
+    """
+    return (REAL_HEIGHT_FT * BLENDER_FLEN_PIXELS) / height_px
 
 
-def estimate_angle(left_height, right_height):
-    if math.fabs(left_height - right_height) <= 1e-6:
+def estimate_angle_deg(left_height_px, right_height_px):
+    if math.fabs(left_height_px - right_height_px) <= 1e-6:
         return 90.0
 
-    left_height = get_dist(left_height)
-    right_height = get_dist(right_height)
+    left_height_ft = get_dist_ft(left_height_px)
+    right_height_ft = get_dist_ft(right_height_px)
 
     c = MID_WIDTH_FT
-    a = min(left_height, right_height)
-    b = max(left_height, right_height)
+    a = min(left_height_ft, right_height_ft)
+    b = max(left_height_ft, right_height_ft)
 
-    cos_angle = (b * b + c * c - a * a) / (2 * b * c)
+    cos_angle = (b * b + c * c - a * a) / (2 * b * c) # TODO: figure out derivation which I need a notebook for
     angle = math.degrees(math.acos(cos_angle))
     return angle
 
 
 def harris_test():
+    """
+    harris is an algorithm to detect corners. However, we decided to use contours, instead.
+    :return:
+    """
     base_image = cv2.imread("./2019_vision_sample.png")  # TODO: fix
 
     base_img_gray = cv2.cvtColor(base_image, cv2.COLOR_BGR2GRAY)
 
-    dst = cv2.cornerHarris(base_img_gray, 2, 3, 0.15)
+    dst = cv2.cornerHarris(src=base_img_gray, blockSize=2, ksize=3, k=0.15)
     harrisy = np.zeros(dst.shape)
     print(harrisy.shape)
 
@@ -93,6 +113,10 @@ def harris_test():
 
 
 def harris_test2():
+    """
+    harris is an algorithm to detect corners. However, we decided to use contours, instead.
+    :return:
+    """
     format_string = os.path.join(AUTOGEN_IMAGE_FOLDER, "2019_vision_angle_{0:0.2f}.png")
 
     angle = 30.0
@@ -151,6 +175,13 @@ def get_contours(image):
 
 
 def get_landmark_points(image):
+    """
+    Take all contours and map to both their lowest and heightest (y-value) points
+
+    Optimally will be four points TODO: right?
+    :param image:
+    :return:
+    """
     contours = get_contours(image)
     tops = [min(contour, key=lambda x: x[1]) for contour in contours]
     bots = [max(contour, key=lambda x: x[1]) for contour in contours]
@@ -159,10 +190,6 @@ def get_landmark_points(image):
 
 
 def calculate_angle(image):
-    # img_path = "/home/ritikm/Pictures/2019_frc/autogen_images/2019_vision_angle_91.50.png"
-
-    # image = cv2.imread(img_path)
-
     contours = get_contours(image)
 
     tops = [min(cnt, key=lambda c: c[1]) for cnt in contours]
@@ -182,20 +209,19 @@ def calculate_angle(image):
     print("Bottoms: ", bottoms)
     # print("Height: ", )
     # print("Reference: 211")
-    angle = estimate_angle(left_height, right_height)
+    angle = estimate_angle_deg(left_height, right_height)
     # print("Estimated angle: ", angle)
     return angle
 
 
-def get_height():
-    format_string = os.path.join(AUTOGEN_IMAGE_FOLDER, "2019_vision_angle_{0:0.2f}.png")
+def test_height_error():
+    string_to_format = os.path.join(AUTOGEN_IMAGE_FOLDER, "2019_vision_angle_{0:0.2f}.png")
 
-    angle = 45.0  # * 2
+    angle = 45.0  # The start angle
     delta = 0.5
-    #
-    # print("top, bottom, angle")
+
     while angle <= 150.0:
-        image = cv2.imread(format_string.format(angle))
+        image = cv2.imread(string_to_format.format(angle))
 
         estimated_angle = calculate_angle(image)
 
@@ -206,45 +232,80 @@ def get_height():
         angle += delta
 
 
-def straighten_image():
-    image = cv2.inRange(
-        cv2.imread("2019_frc/2019_vision_sample.png"),
-        (30, 30, 30),
-        (255, 255, 255)
-    )
+def test_straighten_image():
+    """
+    takes a picture of the vision target splits it in half, rotates both pieces of tape so they are vert (and parallel)
+    and merges them back
+    :return:
+    """
 
-    middle = image.shape[1] // 2
-    left_half = image.T[:middle].T
-    right_half = image.T[middle:].T
-    left_half = scipy.ndimage.rotate(left_half, 14.5)
-    right_half = scipy.ndimage.rotate(right_half, -14.5)
+    # Converts to bitmap (I think)
+    image = cv2.inRange(cv2.imread("2019_frc/2019_vision_sample.png"), (30, 30, 30), (255, 255, 255))
 
-    fixed_image = np.concatenate((left_half, right_half), axis=1)
+    middle = image.shape[1] // 2 # (row_num, column_num, channel_num ... if colored)
+
+    img_left_half = image.T[:middle].T
+    img_right_half = image.T[middle:].T
+
+    # Rotates each image half so the tape should be pointing straight up
+    img_left_half = scipy.ndimage.rotate(img_left_half, VISION_TAPE_ANGLE_FROM_VERT_DEG)
+    img_right_half = scipy.ndimage.rotate(img_right_half, -VISION_TAPE_ANGLE_FROM_VERT_DEG)
+
+    # stitches both images back together
+    # note axis=1 corresponds to y-axis
+    fixed_image = np.concatenate((img_left_half, img_right_half), axis=1)
 
     print(fixed_image.shape)
+
+    # We stitched two images together. This creates a jagged line (which OpenCV can think are contours!)
+    # To avoid this, we add a blur
     fixed_image = cv2.medianBlur(fixed_image, 11)
-    cv2.imshow("a", fixed_image)
+
+    # display the image in a window
+    cv2.imshow("straightened image", fixed_image)
+
+    # waits until a key is pressed
     cv2.waitKey()
+
     contours = get_contours(fixed_image)
     print(contours[0].shape)
 
-    tops = [min(contour, key=lambda x: x[1]) for contour in contours]
-    bots = [max(contour, key=lambda x: x[1]) for contour in contours]
+    # Each contour is a list of pixels (x,y) on the contour. We are attempting
+    # to find the min and max y (a[1]) values of the contours
+    # The axis system is dum so we (top is lower value than bottom)
+    tops = [min(contour, key=lambda a: a[1]) for contour in contours]
+    bots = [max(contour, key=lambda a: a[1]) for contour in contours]
 
-    left_height = bots[0][1] - tops[0][1]
+    # The axis system is dum so we (top is lower value than bottom)
+    left_height = bots[0][1] - tops[0][1] # TODO: is this left tape?
 
-    print(get_dist(left_height))
+    print(get_dist_ft(left_height))
 
 
 def denoising_test():
+    """
+    removes noise from images
+    :return:
+    """
     noisy_image = cv2.imread("2019_vision_sample_noisy.png") # TODO: fix
-    opening = cv2.morphologyEx(noisy_image, cv2.MORPH_OPEN, np.ones((5, 5)))
+
+    # https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_imgproc/py_morphological_ops/py_morphological_ops.html
+    # opening is useful for removing noise (erodes and then dilates)
+    # TODO: how does this even work if we don't have a bit mask yet...
+    opening = cv2.morphologyEx(noisy_image, cv2.MORPH_OPEN, np.ones((5, 5))) # TODO: wot
+
+    # need to dilate because the image has holes in it
+    # TODO: why dilate ==> bitmask ==> dilate when can just bitmask ==> dilate
     dilation = cv2.dilate(opening, np.ones((6, 6)))
+
+    # bitmask anything that remains
     bitmask = cv2.inRange(dilation, (1, 1, 1), (255, 255, 255))
+
     dilation2 = cv2.dilate(bitmask, np.ones((6, 6)), iterations=2)
-    cv2.imshow('dst', dilation2)
+
+    cv2.imshow('dilation 2', dilation2)
     cv2.waitKey()
-    cv2.imwrite("abcd.png", dilation)
+    cv2.imwrite("abcd.png", dilation) # TODO: this can be better...
 
 
 def pose_estimator(): # TODO: fix
@@ -254,14 +315,16 @@ def pose_estimator(): # TODO: fix
         (255, 255, 255)
     )
 
-    focal_length = 35 * 0.00328084  # feet
-    sensor_width = 32.0 * 0.00328084  # feet
-    sensor_height = 18.0 * 0.00328084  # feet
-    pixel_width = 1440
-    pixel_height = 720
+    focal_length = BLENDER_FLEN_FT # feet
+    sensor_width = BLENDER_SENSOR_WIDTH_FT # feet
+    sensor_height = BLENDER_SENSOR_HEIGHT_FT  # feet
+
+    pixel_width = BLENDER_PIXEL_HEIGHT
+    pixel_height = BLENDER_PIXEL_WIDTH
+
     landmark_points_pixels = get_landmark_points(image)  # Points A 1-4
     landmark_points = [np.array([sensor_width * wid / pixel_width, sensor_height * height / pixel_height, focal_length])
-                       for wid, height in landmark_points_pixels]
+                       for wid, height in landmark_points_pixels] # TODO: I'm lazy... will look over/doc later
 
     landmark_points_dict = dict()
 
