b77a15640128e6eb2ed2f1f0f34ed076f5d77148
Ritik Mishra <ritik.mishra314@gmail.com>
2019-03-01 12:11:38 -0600

    Add these files that have been sitting around for a while

--- getting_contours_test.py
+++ getting_contours_test.py
@@ -13,18 +13,21 @@ def get_edges(angle):
     image = cv2.imread(string_to_format.format(angle))  # Image based on angle
     # image = cv2.imread("2019_vision_sample_noisy.png") # Noise image for testing
 
+    bitmask = cv2.inRange(image, (30, 30, 30), (255, 255, 255))
+
     # Remove surrounding noise via opening
-    opening = cv2.morphologyEx(image, cv2.MORPH_OPEN, np.ones((5, 5)))
+    # bitmask = cv2.morphologyEx(bitmask, cv2.MORPH_OPEN, np.ones((5, 5)))
 
     # Dilate to clean up the white blocks
-    dilation = cv2.dilate(opening, np.ones((6, 6)))
+    # bitmask = cv2.dilate(bitmask, np.ones((6, 6)))
 
     # Convert to a bitmask
-    bitmask = cv2.inRange(dilation, (1, 1, 1), (255, 255, 255))
-    bitmask = cv2.dilate(bitmask, np.ones((6, 6)), iterations=2)  # We dilate this twice for some reason...
+
+    # bitmask = cv2.dilate(bitmask, np.ones((6, 6)), iterations=2)  # We dilate this twice for some reason...
 
     # Get messy contours
     contours, _ = cv2.findContours(bitmask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
 
     # Loop through contours and draw them
     for cnt in contours:
@@ -32,11 +35,31 @@ def get_edges(angle):
         epsilon = 0.02 * cv2.arcLength(cnt, True) # Tolerance (decrease for a tighter fit if needed)
         approx = (cv2.approxPolyDP(cnt, epsilon, True))
         # Draw the approx
-        cv2.drawContours(image, [approx], -1, (127, 200, 0), 3)
+        rect = (cv2.minAreaRect(cnt))
+        box = cv2.boxPoints(rect)
+        box = np.int0(box)
+        print(box)
+
+        left = min(box, key=lambda row: row[0])
+        right = max(box, key=lambda row: row[0])
+
+        # centroid
+        M = cv2.moments(cnt)
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+
+
+        if left[1] < right[1]:
+            print("right")
+        else:
+            print("left")
+        cv2.drawContours(image, [box], -1, (127, 10, 0), 3)
+        cv2.circle(image, (int(cX), int(cY)), 3, (0, 255, 0), thickness=4)
+        break
 
     cv2.imshow('done', image)
     cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # cv2.destroyAllWindows()
 
 
 i = 0
@@ -45,5 +68,7 @@ while True:
     if i <= 180:
         get_edges(i)
     else:
+        cv2.destroyAllWindows()
         sys.exit()
+
 
--- test.py
+++ test.py
@@ -2,11 +2,18 @@ import cv2
 import time
 
 import constants
+from pipeline import load_calibration_results
 
 cap = cv2.VideoCapture(constants.CAMERA_ID)
+calib_info = load_calibration_results("testtest.pickle")
 
+print(calib_info.dist_coeffs)
 while True:
     start = time.time()
     ret, frame = cap.read()
-    print("fps:", 1/(time.time() - start))
+    undistort = cv2.fisheye.undistortImage(frame, calib_info.camera_matrix, calib_info.dist_coeffs)
+    cv2.imshow("orig", frame)
+    cv2.imshow("new", undistort)
+    cv2.imshow("avg", (frame + undistort) / (255 * 2))
+    cv2.waitKey(1000 // 30)
 
